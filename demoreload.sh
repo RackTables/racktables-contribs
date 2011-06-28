7a8b1376fd281765e6d5aac2b3dcde6979ff02a5
Denis Ovsienko <infrastation@yandex.ru>
2011-06-28 16:58:31 +0000

    rewrite to support 0.19 branch
    git-svn-id: https://racktables.svn.sourceforge.net/svnroot/racktables@4624

--- demoreload.sh
+++ demoreload.sh
@@ -2,9 +2,9 @@
 
 # This shell script reloads a RackTables demo database. It requires:
 # 1. unpacked RackTables release tarball in home directory
-# 2. init-auth annex files and dictdump.php helper script (normally
-#    already present in contribs directory)
-# 3. optional file with XKCD art database in home directory
+# 2a. init-auth annex files and dictdump.php helper script (normally
+#    already present in contribs directory) for versions 0.16~0.18
+# 2b. init-full annex file in contribs directory for version 0.19
 
 usage()
 {
@@ -12,32 +12,91 @@ usage()
 	exit 1
 }
 
-V=${1:-0.18.5}
-# 0_18_5
+# should work for versions 16, 17, 18
+do_version_16()
+{
+	[ -d "$HOME/RackTables-$V" ] || {
+		echo "$HOME/RackTables-$V does not exist"
+		exit 2
+	}
+	# cd is necessary for dictdump.php to work
+	cd "$HOME/RackTables-$V" || {
+		echo "Failed changing directory to $HOME/RackTables-$V"
+		exit 3
+	}
+	[ -s "install/init-structure.sql" -a -s "install/init-dictbase.sql" ] || {
+		echo "Dump files don't exist"
+		exit 4
+	}
+	if [ "$DODEMO" = "yes" ]; then
+		[ -s install/init-sample-racks.sql ] || {
+			echo "Dump file install/init-sample-racks.sql doesn't exist"
+			exit 7
+		}
+	fi
+	SQLFILE=`mktemp /tmp/demoreload.XXXXXX`
+	cat install/init-structure.sql > "$SQLFILE"
+	cat install/init-dictbase.sql >> "$SQLFILE"
+	if [ -s "$MYDIR/init-auth-$Vx.sql" ]; then
+		cat "$MYDIR/init-auth-$Vx.sql" >> "$SQLFILE"
+	elif [ -s "$MYDIR/init-auth-$V.sql" ]; then
+		cat "$MYDIR/init-auth-$V.sql" >> "$SQLFILE"
+	else
+		echo "neither init-auth-$Vx.sql nor init-auth-$V.sql exist in '$MYDIR'"
+		rm -f "$SQLFILE"
+		exit 5
+	fi
+	$MYDIR/dictdump.php >> "$SQLFILE" || {
+		echo "dictdump.php failed"
+		rm -f "$SQLFILE"
+		exit 6
+	}
+	[ "$DODEMO" = "yes" ] && cat install/init-sample-racks.sql >> "$SQLFILE"
+	# At this point the file is likely to be built Ok, go on with the actual changes.
+	echo "DROP DATABASE IF EXISTS $DB; CREATE DATABASE $DB CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql information_schema
+	mysql $DB < "$SQLFILE"
+	rm -f "$SQLFILE"
+}
+
+do_version_19()
+{
+	[ -s "$MYDIR/init-full-$V.sql" ] || {
+		echo "Dump file install/init-full-$V.sql doesn't exist"
+		exit 2
+	}
+	if [ "$DODEMO" = "yes" ]; then
+		[ -s $HOME/RackTables-$V/scripts/init-sample-racks.sql ] || {
+			echo "Dump file install/init-sample-racks.sql doesn't exist"
+			exit 7
+		}
+	fi
+	SQLFILE=`mktemp /tmp/demoreload.XXXXXX`
+	cat "$MYDIR/init-full-$V.sql" > "$SQLFILE"
+	cat $HOME/RackTables-$V/scripts/init-sample-racks.sql >> "$SQLFILE"
+	echo "DROP DATABASE IF EXISTS $DB; CREATE DATABASE $DB CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql information_schema
+	mysql $DB < "$SQLFILE"
+	rm -f "$SQLFILE"
+}
+
+V=${1:-0.19.4}
+# 0_XX_Y
 Vu=${V//./_}
-# 0.18.x
+# 0.XX.x
 Vx=${V%.[0-9]*}.x
 DB=${2:-demo_$Vu}
 DODEMO=${3:-yes}
 MYNAME=`readlink -f $0`
 MYDIR=`dirname $MYNAME`
 
-cd "$HOME/RackTables-$V" || exit 2
+case $Vx in
+	0.16.x|0.17.x|0.18.x)
+		do_version_16
+		;;
+	0.19.x)
+		do_version_19
+		;;
+	*)
+		echo "This script does not support version $Vx"
+		exit 1
+esac
 
-echo "DROP DATABASE IF EXISTS $DB; CREATE DATABASE $DB CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql information_schema
-echo "source install/init-structure.sql" | mysql $DB
-echo "source install/init-dictbase.sql" | mysql $DB
-if [ -s "$MYDIR/init-auth-$Vx.sql" ]; then
-	echo "source $MYDIR/init-auth-$Vx.sql" | mysql $DB
-elif [ -s "$MYDIR/init-auth-$V.sql" ]; then
-	echo "source $MYDIR/init-auth-$V.sql" | mysql $DB
-else
-	echo "neither init-auth-$Vx.sql nor init-auth-$V.sql exist in '$MYDIR'"
-	exit 3
-fi
-$MYDIR/dictdump.php | mysql $DB
-
-[ "$DODEMO" = "yes" ] || exit
-echo "source install/init-sample-racks.sql" | mysql $DB
-[ ${V#0.16} = $V ] || exit
-[ -s "$HOME/RackTables-XKCD-art-569.sql" ] && echo "source $HOME/RackTables-XKCD-art-569.sql" | mysql $DB
