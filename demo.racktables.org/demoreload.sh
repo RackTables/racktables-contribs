df47dac0ed561991815b373641c8974e63b0f30d
Denis Ovsienko <denis@ovsienko.info>
2017-03-01 16:02:04 +0000

    demo: remove support for versions 0.16.x~0.19.x

--- demo.racktables.org/demoreload.sh
+++ demo.racktables.org/demoreload.sh
@@ -2,14 +2,12 @@
 
 # This shell script reloads a RackTables demo database. It requires:
 # 1. unpacked RackTables release tarball in home directory
-# 2a. init-auth annex files and dictdump.php helper script (normally
-#    already present in contribs directory) for versions 0.16~0.18
-# 2b. init-full annex file in contribs directory for version 0.19
+# 2. init-full extra file in the current directory for version 0.20
 #
 # init-full files are produced by installing RackTables from scratch
 # with HTTP installer and then dumping the database this way:
 # mysqldump --extended-insert=FALSE --order-by-primary racktables_db > init-full-X.Y.Z.sql
-# (after that make sure, that Script table is filled with values
+# (after that make sure that Script table is filled with values
 # appropriate for the demo).
 
 usage()
@@ -18,53 +16,7 @@ usage()
 	exit 1
 }
 
-# should work for versions 16, 17, 18
-do_version_16()
-{
-	[ -d "$HOME/RackTables-$V" ] || {
-		echo "$HOME/RackTables-$V does not exist"
-		exit 2
-	}
-	# cd is necessary for dictdump.php to work
-	cd "$HOME/RackTables-$V" || {
-		echo "Failed changing directory to $HOME/RackTables-$V"
-		exit 3
-	}
-	[ -s "install/init-structure.sql" -a -s "install/init-dictbase.sql" ] || {
-		echo "Dump files don't exist"
-		exit 4
-	}
-	if [ "$DODEMO" = "yes" ]; then
-		[ -s install/init-sample-racks.sql ] || {
-			echo "Dump file install/init-sample-racks.sql doesn't exist"
-			exit 7
-		}
-	fi
-	SQLFILE=`mktemp /tmp/demoreload.XXXXXX`
-	cat install/init-structure.sql > "$SQLFILE"
-	cat install/init-dictbase.sql >> "$SQLFILE"
-	if [ -s "$MYDIR/init-auth-$Vx.sql" ]; then
-		cat "$MYDIR/init-auth-$Vx.sql" >> "$SQLFILE"
-	elif [ -s "$MYDIR/init-auth-$V.sql" ]; then
-		cat "$MYDIR/init-auth-$V.sql" >> "$SQLFILE"
-	else
-		echo "neither init-auth-$Vx.sql nor init-auth-$V.sql exist in '$MYDIR'"
-		rm -f "$SQLFILE"
-		exit 5
-	fi
-	$MYDIR/dictdump.php >> "$SQLFILE" || {
-		echo "dictdump.php failed"
-		rm -f "$SQLFILE"
-		exit 6
-	}
-	[ "$DODEMO" = "yes" ] && cat install/init-sample-racks.sql >> "$SQLFILE"
-	# At this point the file is likely to be built Ok, go on with the actual changes.
-	echo "DROP DATABASE IF EXISTS $DB; CREATE DATABASE $DB CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql information_schema
-	mysql $DB < "$SQLFILE"
-	rm -f "$SQLFILE"
-}
-
-do_version_19()
+do_version_20()
 {
 	[ -s "$MYDIR/init-full-$V.sql" ] || {
 		echo "Dump file $MYDIR/init-full-$V.sql doesn't exist"
@@ -85,7 +37,7 @@ do_version_19()
 	rm -f "$SQLFILE"
 }
 
-V=${1:-0.19.4}
+V=${1:-0.20.12}
 # 0_XX_Y
 Vu=${V//./_}
 # 0.XX.x
@@ -96,11 +48,8 @@ MYNAME=`readlink -f $0`
 MYDIR=`dirname $MYNAME`
 
 case $Vx in
-	0.16.x|0.17.x|0.18.x)
-		do_version_16
-		;;
-	0.19.x|0.20.x)
-		do_version_19
+	0.20.x)
+		do_version_20
 		;;
 	*)
 		echo "This script does not support version $Vx"
--- demo.racktables.org/init-auth-0.16.x.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `UserAccount` (`user_id`, `user_name`, `user_enabled`, `user_password_hash`, `user_realname`)
-VALUES (1,'admin','yes', sha1('admin'),'RackTables Administrator');
